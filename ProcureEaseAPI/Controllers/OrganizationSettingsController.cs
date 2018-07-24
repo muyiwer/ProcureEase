6907942c353f9c8c5f15fb1b95212ac482b27557
Anita Ajekuko <annieajeks@gmail.com>
2018-07-24 14:17:55 +0100

    Implementing API for 'Setup' - Created SourceOfFunds endpoint - Created ProjectCategory endpoint - Created ProcurementMethod endpoint - Created UpdateBasicDetails endpoint

---
+++ ProcureEaseAPI/Controllers/OrganizationSettingsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProcureEaseAPI.Models;
+
+namespace ProcureEaseAPI.Controllers
+{
+    public class OrganizationSettingsController : Controller
+    {
+        private ProcureEaseEntities db = new ProcureEaseEntities();
+
+        // GET: OrganizationSettings
+        public ActionResult Index()
+        {
+            return View(db.OrganizationSettings.ToList());
+        }
+
+        // GET: OrganizationSettings/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrganizationSettings organizationSettings = db.OrganizationSettings.Find(id);
+            if (organizationSettings == null)
+            {
+                return HttpNotFound();
+            }
+            return View(organizationSettings);
+        }
+
+        // GET: OrganizationSettings/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrganizationSettings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OrganizationID,OrganizationNameInFull,OrganizationNameAbbreviation,OrganizationEmail,Address,Country,State,AboutOrganization,DateModified,CreatedBy,DateCreated,OrganizationLogoPath")] OrganizationSettings organizationSettings)
+        {
+            if (ModelState.IsValid)
+            {
+                organizationSettings.OrganizationID = Guid.NewGuid();
+                db.OrganizationSettings.Add(organizationSettings);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(organizationSettings);
+        }
+
+        // GET: OrganizationSettings/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrganizationSettings organizationSettings = db.OrganizationSettings.Find(id);
+            if (organizationSettings == null)
+            {
+                return HttpNotFound();
+            }
+            return View(organizationSettings);
+        }
+
+        // POST: OrganizationSettings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OrganizationID,OrganizationNameInFull,OrganizationNameAbbreviation,OrganizationEmail,Address,Country,State,AboutOrganization,DateModified,CreatedBy,DateCreated,OrganizationLogoPath")] OrganizationSettings organizationSettings)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(organizationSettings).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(organizationSettings);
+        }
+
+        // GET: OrganizationSettings/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrganizationSettings organizationSettings = db.OrganizationSettings.Find(id);
+            if (organizationSettings == null)
+            {
+                return HttpNotFound();
+            }
+            return View(organizationSettings);
+        }
+
+        // POST: OrganizationSettings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            OrganizationSettings organizationSettings = db.OrganizationSettings.Find(id);
+            db.OrganizationSettings.Remove(organizationSettings);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

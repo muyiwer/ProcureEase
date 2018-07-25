a3fe7e7eda649ac670e404e0f9476b2cc2c6d9e8
Aro Olumuyiwa <muyiweraro@gmail.com>
2018-07-25 19:36:09 +0100

    Made changes to UserController(password reset end points) EmailTemplateHelper(added signup  and password reset template)

---
+++ ProcureEaseAPI.Tests/Controllers/UserTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProcureEaseAPI.Controllers;
+using ProcureEaseAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcureEaseAPI.Tests.Controllers
+{
+    [TestClass]
+   public  class UserTest
+    {
+        [TestMethod]
+        public void TestAddUser()
+        {
+            UserProfile UserProfile = new UserProfile
+            {
+                UserEmail = "muyiweraro@gmail.com"
+            };      
+            var testAddUser = new UsersController();
+            var result = testAddUser.Add(UserProfile);
+            Assert.IsNull(result);
+            
+        }
+
+        [TestMethod]
+        public void TestInitiatePasswordReset()
+        {
+            string UserEmail = "muyiweraro@gmail.com";           
+            var testAddUser = new UsersController();
+            var result = testAddUser.InitiatePasswordReset(UserEmail);
+            Assert.IsNotNull(result);
+        }
+
+        //[TestMethod]
+        //public void TestPasswordReset()
+        //{
+        //    ResetPasswordModel ResetPassword = new ResetPasswordModel
+        //    {
+        //        UserEmail = "muyiweraro@gmail.com",
+        //        NewPassword ="",
+        //        ResetToken ="",
+        //    };
+        //    var testAddUser = new UsersController();
+        //    var result = testAddUser.ResetPassword(ResetPassword);
+        //    Assert.IsNotNull(result);
+
+        //}
+    }
+}

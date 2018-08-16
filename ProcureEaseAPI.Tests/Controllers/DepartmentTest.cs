879f61b2bdbedd4923831f7d51d96a61b8448215
Anita Ajekuko <annieajeks@gmail.com>
2018-08-16 09:45:24 +0100

    Updated Department API Test

--- ProcureEaseAPI.Tests/Controllers/DepartmentTest.cs
+++ ProcureEaseAPI.Tests/Controllers/DepartmentTest.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace ProcureEaseAPI.Tests.Controllers
 {
     [TestClass]
-    class DepartmentTest
+    public class DepartmentTest
     {
         [TestMethod]
         public void TestAddDepartment()

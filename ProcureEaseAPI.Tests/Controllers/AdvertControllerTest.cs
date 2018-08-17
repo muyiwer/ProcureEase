ecc9e49ae6ea84280534fac4835079dd0aafcdec
Anita Ajekuko <annieajeks@gmail.com>
2018-08-17 15:02:40 +0100

    Updated Advert Test Controller -Implemented test to get Advert Summary -Implemented test to get Advert Details

--- ProcureEaseAPI.Tests/Controllers/AdvertControllerTest.cs
+++ ProcureEaseAPI.Tests/Controllers/AdvertControllerTest.cs
@@ -1,13 +1,40 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProcureEaseAPI.Controllers;
+using ProcureEaseAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http.Results;
+using System.Web.Mvc;
 
 namespace ProcureEaseAPI.Tests.Controllers
 {
+    [TestClass]
     public class AdvertControllerTest
     {
+        [TestMethod]
+        public void TestGetAdvertSummay()
+        {
+            Adverts adverts = new Adverts
+            {
 
+            };
+            var testGetAdvertSummay = new AdvertsController();
+            var result = (JsonResult)testGetAdvertSummay.AdvertSummary();
+            Console.WriteLine(result.Data);
+            Assert.IsTrue((result.Data + "").Contains("Advert Summary"));
+        }
+
+        [TestMethod]
+        public void TestGetAdvertDetails()
+        {
+            Guid id = new Guid("B9571BD0-BCBE-4817-A22E-41D1FC9E82FD");        
+            var testGetAdvertDetails = new AdvertsController();
+            var result = (JsonResult)testGetAdvertDetails.AdvertDetails(id);
+            Console.WriteLine(result.Data);
+            Assert.IsTrue((result.Data + "").Contains("Advert details"));
+        }
     }
 }

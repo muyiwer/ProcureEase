057112e7324099e7421bb71185ce9e899fbcbbdd
Aro Olumuyiwa <muyiweraro@gmail.com>
2018-10-05 15:10:41 +0100

    added cors configuration on webConfig and Global.asax

--- ProcureEaseAPI/Global.asax.cs
+++ ProcureEaseAPI/Global.asax.cs
@@ -25,8 +25,8 @@ namespace ProcureEaseAPI
             if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
             {
                 HttpContext.Current.Response.AddHeader("Cache-Control", "no-cache");
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, MERGE");
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS,MERGE");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept, Email");
                 HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
                 HttpContext.Current.Response.End();
 
